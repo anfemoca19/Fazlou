a5cb93dfaed6708b292a128accb87fa88bd2da24
CrisOspina <crisospina523@gmail.com>
2019-09-28 03:50:15 -0500

    [update]

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
+// import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/2.jpg'
 import pic03 from '../images/3.jpg'
 import foto from '../images/1.jpg'
@@ -21,30 +21,30 @@ class Main extends React.Component {
         id="main"
         style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
       >
-        
         <article
           id="intro"
           className={`${this.props.article === 'intro' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
           }`}
-          style={{ display: 'none', textAlign:'justify'}}
+          style={{ display: 'none', textAlign: 'justify' }}
         >
           <h2 className="major">Introduccion</h2>
           <span className="image main">
-            <img src={foto} style={{}}alt="" />
-            
+            <img src={foto} style={{}} alt="" />
           </span>
           <h2>MISION</h2>
           <p>
-          La misión de Fazlou es ayudar a las personas y las empresas en la definición y seguimientos de Metas u objetivos, por medio de
-          una página web amigable al usuario y de fácil manejo.
+            La misión de Fazlou es ayudar a las personas y las empresas en la
+            definición y seguimientos de Metas u objetivos, por medio de una
+            página web amigable al usuario y de fácil manejo.
           </p>
-           
+
           <h2>VISION</h2>
           <p>
-          La visión de Fazlou es ser la mejor página web en la ayuda de la planificación de objetivos y metas de todo el mundo para el 2024. 
+            La visión de Fazlou es ser la mejor página web en la ayuda de la
+            planificación de objetivos y metas de todo el mundo para el 2024.
           </p>
-        
+
           {close}
         </article>
 
@@ -57,28 +57,41 @@ class Main extends React.Component {
         >
           <h2 className="major">ALCANZA TUS SUEÑOS</h2>
           <span className="image main">
-            <img src={pic02} alt="" /> 
+            <img src={pic02} alt="" />
           </span>
           <h2>COMO DEFINIR TUS METAS</h2>
 
-             
-             <form method="post" action="#">
-              <div> {/*className="field half first" */}
-                <label htmlFor="name">Cual es tu meta </label>
-                <input type="text" name="name" id="name" placeholder="Ejemplo: Escribe aquí tus sueños o metas que quieras alcanzar en tu vida." />
-              </div>
-              
-              <div> {/*className="field half"*/}
-                <label htmlFor="email">Categoria de la meta</label>
-                <input type="text" name="email" id="email" placeholder="Ejemplo: Laboral, Familiar, Educativo, Espiritual, Financiero, Deportivo." />
-              </div>
+          <form method="post" action="#">
+            <div>
+              {' '}
+              {/*className="field half first" */}
+              <label htmlFor="name">Cual es tu meta </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Ejemplo: Escribe aquí tus sueños o metas que quieras alcanzar en tu vida."
+              />
+            </div>
 
-              <div className="field">
-                <label htmlFor="message">Por que</label>
-                <textarea name="message" id="message" rows="4"></textarea>
-              </div>
+            <div>
+              {' '}
+              {/*className="field half"*/}
+              <label htmlFor="email">Categoria de la meta</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Ejemplo: Laboral, Familiar, Educativo, Espiritual, Financiero, Deportivo."
+              />
+            </div>
 
-              <div class="form-group col-md-4">
+            <div className="field">
+              <label htmlFor="message">Por que</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+
+            <div class="form-group col-md-4">
               <label for="inputState">Plazo</label>
               <select id="inputState" class="form-control">
                 <option selected> </option>
@@ -86,39 +99,38 @@ class Main extends React.Component {
                 <option>Mediano </option>
                 <option>Largo</option>
               </select>
-              </div>
+            </div>
 
-              <div class="form-group col-md-4">
+            <div class="form-group col-md-4">
               <label for="inputState">Prioridad</label>
-                <select id="inputState" class="form-control">
-                  <option selected> </option>
-                  <option>Alta</option>
-                  <option>Media </option>
-                  <option>Baja</option>
-                </select>
-               </div>
-               <div class="form-group col-md-4">
+              <select id="inputState" class="form-control">
+                <option selected> </option>
+                <option>Alta</option>
+                <option>Media </option>
+                <option>Baja</option>
+              </select>
+            </div>
+            <div class="form-group col-md-4">
               <label for="inputState">Complejidad</label>
-                <select id="inputState" class="form-control">
-                  <option selected> </option>
-                  <option>Alta</option>
-                  <option>Media alta </option>
-                  <option>Media </option>
-                  <option>Media baja</option>
-                  <option>Baja</option>
-                </select>
-               </div>
-              <ul className="actions">
+              <select id="inputState" class="form-control">
+                <option selected> </option>
+                <option>Alta</option>
+                <option>Media alta </option>
+                <option>Media </option>
+                <option>Media baja</option>
+                <option>Baja</option>
+              </select>
+            </div>
+            <ul className="actions">
               <li>
                 <input type="submit" value="ENVIAR" className="special" />
               </li>
               <li>
                 <input type="reset" value="LIMPIAR" />
               </li>
-              </ul>
+            </ul>
           </form>
 
-             
           {close}
         </article>
 
@@ -133,9 +145,7 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic03} alt="" />
           </span>
-          <p>
-          
-          </p>
+          <p></p>
           {close}
         </article>
         <article
@@ -149,9 +159,7 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic03} alt="" />
           </span>
-          <p>
-         
-          </p>
+          <p></p>
           {close}
         </article>
         <article
@@ -194,20 +202,23 @@ class Main extends React.Component {
               </a>
             </li>
             <li>
-              <a href="https://m.facebook.com/home.php?_rdr" className="icon fa-facebook">
+              <a
+                href="https://m.facebook.com/home.php?_rdr"
+                className="icon fa-facebook"
+              >
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://www.instagram.com/fazlou98/" className="icon fa-instagram">
+              <a
+                href="https://www.instagram.com/fazlou98/"
+                className="icon fa-instagram"
+              >
                 <span className="label">Instagram</span>
               </a>
             </li>
             <li>
-              <a
-                href="https://github.com/"
-                className="icon fa-github"
-              >
+              <a href="https://github.com/" className="icon fa-github">
                 <span className="label">GitHub</span>
               </a>
             </li>
