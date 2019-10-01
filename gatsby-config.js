0eff9966f36808517f6b430ecfeab051e090f906
CrisOspina <crisospina523@gmail.com>
2019-10-01 17:32:22 -0500

    [update] name-color-pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Fazlou',
+        short_name: 'Fazlou-beta',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#23222C',
+        theme_color: '#23222C',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
